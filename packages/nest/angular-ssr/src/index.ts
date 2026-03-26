e80f786d87b01f2b4399e548b39f2e061962a97c
Johan Vrolix <vrxj81@users.noreply.github.com>
2026-03-26 11:41:19 +0100

    feat(nest-angular-ssr): add explicit Nest SSR bootstrap helper (#26)
    Closes #20
    Refs #14

--- packages/nest/angular-ssr/src/index.ts
+++ packages/nest/angular-ssr/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/core/angular-ssr-contract.js';
 export * from './lib/core/angular-node-ssr-renderer.js';
+export * from './lib/nest/nest-angular-ssr-bootstrap.js';
 export * from './lib/nest/nest-angular-ssr-integration.js';
 export * from './lib/nest/nest-angular-ssr-routing.js';
